f527b2844c6f694f4ed9f225357518a281bc4aed
Marko Dupor <marko.dupor@asseco-see.hr>
2020-07-30 09:58:20 +0200

    Config change

--- src/CodeQualityServiceProvider.php
+++ src/CodeQualityServiceProvider.php
@@ -9,23 +9,23 @@ use Voice\CodeQuality\App\Console\Commands\TddCommand;
 class CodeQualityServiceProvider extends ServiceProvider
 {
     /**
+     * Register the application services.
+     */
+    public function register()
+    {
+        $this->mergeConfigFrom(__DIR__ . '/config/asseco-code-quality.php', 'asseco-code-quality');
+    }
+
+    /**
      * Bootstrap the application services.
      */
     public function boot()
     {
-        $this->publishes([__DIR__ . '/config/asseco-voice.php' => config_path('asseco-voice.php')]);
+        $this->publishes([__DIR__ . '/config/asseco-code-quality.php' => config_path('asseco-code-quality.php')]);
 
         $this->commands([
             GitHooksCommand::class,
             TddCommand::class
         ]);
-    }
-
-    /**
-     * Register the application services.
-     */
-    public function register()
-    {
-        $this->mergeConfigFrom(__DIR__ . '/config/asseco-voice.php', 'asseco-voice');
     }
 }
--- src/config/asseco-voice.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    'code_quality' => [
-
-    ]
-];
